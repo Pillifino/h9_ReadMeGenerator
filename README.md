4bfef32f64e58cacbd4f4105d85eb9d804f63fbc
Pillifino <dinodizon073100@gmail.com>
2022-04-18 22:14:04 -0700

    Functioning README Generator!

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# h9_ReadMeGenerator
+# <ReadMeGenerator>
+  
+    ## Description
+        It generates ReadMe's for the user
+    ## Installation Instructions
+        None Right Now
+    ## Usage Information
+        It's up to you!
+    ## Credits
+        Fill this in  
+    ## Tests
+        none!
+  
--- index.js
+++ index.js
@@ -1,1 +1,58 @@
-console.log("test");
+const inquirer = require("inquirer"); //
+const fs = require("fs");
+
+const generateReadMe = (data) => //Literally Put Readme outline in here and add ${data.datatypehere}
+  `# <${data.title}>
+  
+    ## Description
+        ${data.description}
+    ## Installation Instructions
+        ${data.installation}
+    ## Usage Information
+        ${data.usage}
+    ## Credits
+        ${data.credits} 
+    ## Tests
+        ${data.tests}
+  `;
+
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "What is your Project Title?",
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Tell Us About Your Project",
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "Installation Instuctions?",
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "How Do We Use This Project?",
+    },
+    {
+      type: "input",
+      name: "credits",
+      message: "Who Helped Out With This Project?",
+    },
+    {
+      type: "input",
+      name: "tests",
+      message: "What Tests Are You Adding?",
+    },
+  ])
+  .then((answers) => {
+    const ReadMEContent = generateReadMe(answers);
+
+    fs.writeFile("README.md", ReadMEContent, (err) =>
+      err ? console.log(err) : console.log("Successfully created README!")
+    );
+  });
