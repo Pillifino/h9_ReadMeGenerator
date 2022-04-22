c45d547355f2ce694e9b6ad3c6f4f77399aea91e
Pillifino <dinodizon073100@gmail.com>
2022-04-21 17:44:21 -0700

    added license fucntion and project README file

---
+++ ReadMeHW.md
@@ -0,0 +1,18 @@
+
+  [![License: CC0-1.0](https://img.shields.io/badge/License-CC0%201.0-lightgrey.svg)](http://creativecommons.org/publicdomain/zero/1.0/)
+  # Project: 
+  blank
+  
+  ## Description
+  blank
+  ## Installation
+  blank
+  ## Instructions
+  blank
+  ## Github Username
+  blank
+  ## Email
+  blank
+  
+  ## License
+  Creative Commons Zero v1.0 Universal
--- index.js
+++ index.js
@@ -1,86 +1,74 @@
-const inquirer = require("inquirer"); //
+// Packages needed for this application
+const inquirer = require("inquirer");
 const fs = require("fs");
+const generateMarkdown = require("./Utils/generateMarkdown");
 
-const generateReadMe = (data) => //Literally Put Readme outline in here and add ${data.datatypehere}
-  `# ${data.title}
- 
-    ## Description
-        ${data.description}
-    ## Installation Instructions
-        ${data.installation}
-    ## Usage Information
-        ${data.usage}
-    ## Credits
-        ${data.credits} 
-    ## Tests
-        ${data.tests}
-    ## License
-        ${data.license}
-    ## Questions
-        https://github.com/${data.username}/
-        For Any Additional Questions Email Me At: ${data.email}    
-  `;
-
-inquirer
-  .prompt([
+// An array of questions for user input
+const questions = [
     {
-      type: "input",
-      name: "title",
-      message: "What is your Project Title?",
-    },
-    {
-      type: "input",
-      name: "description",
-      message: "Tell Us About Your Project",
-    },
-    {
-      type: "input",
-      name: "installation",
-      message: "Installation Instuctions?",
-    },
-    {
-      type: "input",
-      name: "usage",
-      message: "How Do We Use This Project?",
-    },
-    {
-      type: "input",
-      name: "credits",
-      message: "Who Helped Out With This Project?",
-    },
-    {
-      type: "input",
-      name: "tests",
-      message: "What Tests Are You Adding?",
-    },
-    {
-       type:'list',
-       message: "Choose your license for your application:",
-       name: "license",
-       choices: [
-       "Apache License 2.0",
-       "GNU General Public License v3.0",
-       "MIT License",
-       "BSD 2-Clause'Simplified' License",
-       "Creative Commons Zero v1.0 Universal",
-       "Eclipse Public License 2.0",
-       "The Unlicense"]
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?",
     },
     {
         type: "input",
-        name: "username",
-        message: "What is your username?",
-      },
+        name: "description",
+        message: "Describe Your Project.",
+    },
+    {
+        type: "input",
+        name: "install",
+        message: "How do you install your project?",
+    },
+    {
+        type: "input",
+        name: "instructions",
+        message: "How do you use this application?",
+    },
+    {
+        type: "input",
+        name: "gitHubName",
+        message: "What is your Github Username?",
+    },
     {
         type: "input",
         name: "email",
         message: "What is your email?",
-      },
-  ])
-  .then((answers) => {
-    const ReadMEContent = generateReadMe(answers);
+    }, 
+    {
+        type: "checkbox",
+        name: "license",
+        choices: [
+            "Apache License 2.0",
+            "GNU General Public License v3.0",
+            "MIT License",
+            "BSD 2-Clause'Simplified' License",
+            "Creative Commons Zero v1.0 Universal",
+            "Eclipse Public License 2.0",
+            "The Unlicense"
+        ]
+    },
+];
 
-    fs.writeFile("README.md", ReadMEContent, (err) =>
-      err ? console.log(err) : console.log("Successfully created README!")
-    );  
-  });
+// A function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+            console.log("Error: File Not Created");
+        } else {
+            console.log("File created!");
+        }
+    });
+}
+
+// A function to initialize app
+function init() {
+    inquirer.prompt(questions)
+    .then((answers) => {
+        const readMeContent = generateMarkdown(answers);
+        writeToFile("ReadMeHW.md", readMeContent);
+    });
+}
+
+// Function call to initialize app
+init();
